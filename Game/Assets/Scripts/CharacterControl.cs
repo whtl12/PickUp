03f42ca9799980adc6b4eed71d8c4522a08f3e1a
Vaccine <sang10011@naver.com>
2018-05-25 22:55:49 +0900

    game scene demo

---
+++ Game/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+    float h, v;
+    float hSpeed;
+	// Use this for initialization
+	void Start () {
+        h = v = 0;
+        hSpeed = 0.5f;
+        GetComponent<Rigidbody>().maxDepenetrationVelocity = 3;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        h = Input.GetAxis("Horizontal");
+        //GetComponent<Rigidbody>().velocity += new Vector3(h * hSpeed, v);
+        GetComponent<Rigidbody>().AddForce(new Vector3(h * hSpeed, v));
+        if (transform.position.x > 3 || transform.position.x < -3)
+            GetComponent<Rigidbody>().velocity = new Vector3();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        iTween.MoveTo(collision.transform.parent.gameObject, Vector3.down / 5, 1);
+
+        //iTween.MoveBy(gameObject, Vector3.down / 5, 1);
+        //iTween.MoveFrom(gameObject, Vector3.down / 5, 1);
+        //iTween.MoveAdd(gameObject, Vector3.down / 5, 1);
+    }
+}
---
+++ Game/Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    //const float MaxSpawnWidth = 3;
+    //const float MinSpawnWidth = -3;
+    //float IntervalSpawnWidth = 1.5f;
+    //float IntervalSpawnHeight = 3f; // 동적생성 시 사용 변수
+
+    public GameObject prefab1 = null;
+    [HideInInspector]
+    public float speed = .5f;
+    List<GameObject> Obstacle = new List<GameObject>();
+
+    public GameObject InitObstacle()
+    {
+        return Instantiate(prefab1, transform.position, transform.rotation);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Obstacle.Add(InitObstacle());
+    }
+
+    // Update is called once per frame
+    void Update () {
+        for(int i = 0; i < Obstacle.Count; i++)
+        {
+            Obstacle[i].transform.Translate(Vector3.up * speed);
+            if (Obstacle[i].transform.GetChild(Obstacle[i].transform.childCount - 1).position.y > transform.position.y && Obstacle[i].tag != "Finish")
+            {
+                Obstacle.Add(InitObstacle());
+                Obstacle[i].tag = "Finish";
+            }
+            if (Obstacle[i].transform.GetChild(Obstacle[i].transform.childCount - 1).position.y > 10)
+            {
+                Destroy(Obstacle[i]);
+                Obstacle.Remove(Obstacle[i]);
+            }
+        }
+
+
+    }
+}

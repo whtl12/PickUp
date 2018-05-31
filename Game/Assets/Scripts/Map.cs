7c1fe0e5f918778d0c337bad64b4a98ef3ea5678
Vaccine <sang10011@naver.com>
2018-06-01 01:15:40 +0900

    Add BG1

--- Game/Assets/Scripts/Map.cs
+++ Game/Assets/Scripts/Map.cs
@@ -9,24 +9,47 @@ public class Map : MonoBehaviour {
     //float IntervalSpawnHeight = 3f; // 동적생성 시 사용 변수
 
     public GameObject prefab1;
+    public GameObject BGpref;
     //[HideInInspector]
     public float speed;
     List<GameObject> Obstacle = new List<GameObject>();
+    List<GameObject> Background = new List<GameObject>();
 
     public GameObject InitObstacle()
     {
         return Instantiate(prefab1, transform.position, transform.rotation);
     }
+    public GameObject InitBackground(int init = -30)
+    {
+        GameObject instance = Instantiate(BGpref, new Vector3(0, init), Quaternion.Euler(0, 180, 0));
+        instance.transform.localScale = new Vector3(1, 2, 1);
+        return instance;
+    }
 
     // Use this for initialization
     void Start () {
         Obstacle.Add(InitObstacle());
+        Background.Add(InitBackground(0));
         speed = 0.05f;
     }
 
     // Update is called once per frame
     void Update () {
-        for(int i = 0; i < Obstacle.Count; i++)
+        for(int i = 0; i < Background.Count; i++)
+        {
+            Background[i].transform.Translate(Vector3.up * speed);
+            if (Background[i].transform.position.y > 1 && Background[i].tag != "Finish")
+            {
+                Background.Add(InitBackground());
+                Background[i].tag = "Finish";
+            }
+            if (Background[i].transform.position.y > 30)
+            {
+                Destroy(Background[i]);
+                Background.Remove(Background[i]);
+            }
+        }
+        for (int i = 0; i < Obstacle.Count; i++)
         {
             Obstacle[i].transform.Translate(Vector3.up * speed);
             if (Obstacle[i].transform.GetChild(Obstacle[i].transform.childCount - 1).position.y > transform.position.y && Obstacle[i].tag != "Finish")

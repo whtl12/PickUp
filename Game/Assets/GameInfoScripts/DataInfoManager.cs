ed6820f86a28573e9d71a4d96a5a5fb7f1568a3a
Vaccine <sang10011@naver.com>
2018-06-06 21:40:16 +0900

    Revert "독스값이 안불러와져"
    This reverts commit c0df1b63ac698cd0f95bfcfcde73e79a87f46900.

--- Game/Assets/GameInfoScripts/DataInfoManager.cs
+++ Game/Assets/GameInfoScripts/DataInfoManager.cs
@@ -18,7 +18,9 @@ public class DataInfoManager : MonoBehaviour
     private void Awake()
     {
         if (m_Instance == null)
+        {
             m_Instance = this;
+        }
         else if (m_Instance != this)
             Destroy(gameObject);
 
@@ -56,14 +58,11 @@ public class DataInfoManager : MonoBehaviour
         }
 
         SkillDataInfoTable.GetDictionary<SkillInfo>();
-        CharacterDataInfoTable.GetDictionary<CharacterInfo>();
 
         //로드 끝나고 씬 바꾸기.
         //처음 로고 영상? 보여주고 씬넘기기 위한 특수 경우기때문에 그냥 넘김.
 
         if (SceneManager.GetActiveScene().buildIndex == (int)UIManager.SceneLoadIndex.Intro)
             SceneManager.LoadScene((int)UIManager.SceneLoadIndex.Start);
-
-        print(CharacterDataInfoTable.dicCharacterinfoTable[0].vSpeed);
     }
 }
